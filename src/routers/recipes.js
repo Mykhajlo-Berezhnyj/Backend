e2af48efbf5ac9756b25854bbfd5f9b89b9f4379
Sasha <malinka15117kk@gmail.com>
2025-08-21 20:28:18 +0300

    lol

--- src/routers/recipes.js
+++ src/routers/recipes.js
@@ -1,16 +1,9 @@
 import { Router } from "express";
-import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { isValidId } from "../middlewares/isValidId.js";
-import { authenticate } from "../middlewares/authenticate.js";
-import { addFavoriteRecipeController } from "../controllers/recipes.js";
+// import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+// import { isValidId } from "../middlewares/isValidId.js";
+// import { authenticate } from "../middlewares/authenticate.js";
+// import { addFavoriteRecipeController } from "../controllers/recipes.js";
 
 const router = Router();
-
-router.post(
-  "/favorites/:recipeId",
-  isValidId,
-  authenticate,
-  ctrlWrapper(addFavoriteRecipeController)
-);
 
 export default router;
