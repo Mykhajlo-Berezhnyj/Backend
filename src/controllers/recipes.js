10af5023ebcdf0b2dde4705c500006bd8d3619a5
Maksym Drachenko <maks7d@gmail.com>
2025-08-21 16:51:45 +0500

    add favoriteRecipes

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -1,18 +1,31 @@
 import createHttpError from "http-errors";
-import { deleteFavoriteRecipeById } from "../services/recipes.js";
+import {
+  deleteFavoriteRecipeById,
+  getFavoriteRecipes,
+} from "../services/recipes.js";
 
-export const recipesController = async (req, res) => {};
+// export const recipesController = async (req, res) => {};
 
 export const deleteFavoriteRecipeByIdController = async (req, res, next) => {
-    try {
-        const { recipeId } = req.params;
-        if (!recipeId) return next(createHttpError(400, 'Missing recipeId'));
+  try {
+    const { recipeId } = req.params;
+    if (!recipeId) return next(createHttpError(400, "Missing recipeId"));
 
-        const user = await deleteFavoriteRecipeById(recipeId, req.user._id);
-        if (!user) return next(createHttpError(404, 'User not found'));
+    const user = await deleteFavoriteRecipeById(recipeId, req.user._id);
+    if (!user) return next(createHttpError(404, "User not found"));
 
-        res.status(204).send();
-    } catch (error) {
-        next(error);
-    }
-}
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getFavoriteRecipesController = async (req, res, next) => {
+  try {
+    const favoriteRecipes = await getFavoriteRecipes(req.user._id);
+
+    res.status(200).json(favoriteRecipes);
+  } catch (error) {
+    next(error);
+  }
+};
